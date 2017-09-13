8aa62de669c2bad8adeee84c50b798f115adfb82
Marc <outflux3@users.noreply.github.com>
2017-09-13 04:09:27 -0400

    Repeater Support (#8)
    * Repeater Support for Multi
    
    Only worked on the multi repeater support, same basic pattern could be
    used to support chosen single select...
    
    * Repeater Support

--- InputfieldChosenSelect.js
+++ InputfieldChosenSelect.js
@@ -1,13 +1,25 @@
-$(document).ready(function() {
-	$(".InputfieldChosenSelect select:not([multiple])").each(function() {
-		var $t = $(this); 
 
-		if(typeof config === 'undefined') {
-			var options = {};
-		} else {
-			var options = config[$t.attr('id')]; 
-		}
+var initChosen = function() {
+	var $t = $(this); 
+    var tid = $t.attr('id');
+    var oid = tid.split('_repeater')[0];
+
+	if(typeof config === 'undefined') {
+		var options = {};
+	} else {
+		var options = config[oid]; 
+	}
     if($t.not("[required]")) $t.children().first().text("");
-		$t.chosen(options); 
-	}); 
-}); 
+
+	$t.chosen(options); 
+};
+
+$(document).ready(function() {
+
+	$(".InputfieldChosenSelect select:not([multiple])").each(initChosen);
+
+	$(document).on('reloaded opened repeateradd wiretabclick', '.InputfieldPage', function() {
+		$(this).find(".InputfieldChosenSelect select:not([multiple])").each(initChosen);
+	});
+
+});
